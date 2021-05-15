ce6166b090f37e0eff0f8b4cdae21ffec346144f
prashantpandya000 <prashantpandya46@gmail.com>
2021-05-15 11:05:17 +0530

    first commit

---
+++ base/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Home(models.Model):
+    name = models.CharField(max_length=20)
+    greetings_1 = models.CharField(max_length=5)
+    greetings_2 = models.CharField(max_length=5)
+    picture = models.ImageField(upload_to='picture/')
+    # save time when modified
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class About(models.Model):
+    heading = models.CharField(max_length=50)
+    career = models.CharField(max_length=20)
+    description = models.TextField(blank=False)
+    profile_img = models.ImageField(upload_to='profile/')
+    
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.career
+
+
